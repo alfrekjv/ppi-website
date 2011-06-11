b119837abe28d95c4d2401f7a5ecec1766459f21
Paul Dragoonis <dragoonis@gmail.com>
2011-06-11 16:20:36 +0100

    Added baseurl prefix to image includes

--- App/View/default/home/index.php
+++ App/View/default/home/index.php
@@ -18,14 +18,14 @@
                 </div>
                  <div class="item-container">
                     <div class="community item">
-                        <a href="http://groups.google.com/group/ppi-framework/" title="google group"><img src="images/icons-community.png" alt="community"></a>
+                        <a href="http://groups.google.com/group/ppi-framework/" title="google group"><img src="<?php echo $baseUrl; ?>images/icons-community.png" alt="community"></a>
                         <div class="item-content">
                             <h3><a href="http://groups.google.com/group/ppi-framework/" title="google group">community</a></h3>
                             <p>There are a variety of mediums available to interact with, gain support from, or become part of; the PPI framework.  To get started, choose from one of the following: <a href="irc://irc.freenode.net/ppi">IRC</a>, <a href="">Forum</a>, <a href="http://groups.google.com/group/ppi-framework/topics">Mailing List</a>.</p>
     		    </div>
                     </div>
                     <div class="documentation item">
-                        <a href="http://www.ppiframework.com/docs" title=""><img src="images/icons-documentation.png" alt="documentation"></a>
+                        <a href="http://www.ppiframework.com/docs" title=""><img src="<?php echo $baseUrl; ?>images/icons-documentation.png" alt="documentation"></a>
                         <div class="item-content">
                             <h3><a href="http://www.ppiframework.com/docs">documentation</a></h3>
                             <p>Tutorials are the best way to get started with PPI. It explains everything you need to know; How to wield its many features to develop web applications faster and more efficiently, even if you only know a little bit of PHP.</p>
@@ -34,14 +34,14 @@
                 </div>
                 <div class="item-container">
                     <div class="development item">
-                        <a href="http://www.ppiframework.com/support" title="Development"><img src="images/icons-development.png" alt="development"></a>
+                        <a href="http://www.ppiframework.com/support" title="Development"><img src="<?php echo $baseUrl; ?>images/icons-development.png" alt="development"></a>
                         <div class="item-content">
                             <h3><a href="http://www.ppiframework.com/support" title="Development">development</a></h3>
                             <p>Want to get involved in the project? We're looking for designers, front-end developers and back-end developers. We will always have something to suit your mood. You can pick up a ticket on our <a href="http://www.ppiframework.com/support" title="Issue Tracker">Issue Tracker</a> and get stuck in.</p>
                         </div>
                     </div>
                     <div class="github item">
-                        <a href="http://github.com/dragoonis"><img src="images/icons-github.png" alt="github"></a>
+                        <a href="http://github.com/dragoonis"><img src="<?php echo $baseUrl; ?>images/icons-github.png" alt="github"></a>
                         <div class="item-content">
                             <h3><a href="http://github.com/dragoonis" title="github">github</a></h3>
                             <p>Clone one of our projects, create a <a href="http://help.github.com/forking" title="fork">fork</a>, submit a <a href="http://help.github.com/pull-requests" title="pull request">pull request</a>, that's how we do it in github-land. Whether you're looking to just grab the latest version of the code, <a href="https://github.com/dragoonis/PPI-Framework/watchers" title="">follow the github project</a> or have a snoop about, you can find it all <a href="https://github.com/dragoonis" title="github">here</a>.</p>
