ee43ad80fef5530e441d0650f3dd7bb37baccfee
Alfredo Juarez <alfrekjv@gmail.com>
2011-08-05 09:51:40 -0600

    changing id for classes on filters

--- App/View/default/community/index.php
+++ App/View/default/community/index.php
@@ -8,7 +8,7 @@
     <div class="content-box">
 		<h1>Activity Stream</h1>
         <div class="topcontent">
-                <div class="filter">Filter: <a href="<?= $baseUrl; ?>community/" id="filter-all">All</a> - <a href="<?= $baseUrl; ?>community/index/filter/twitter" id="filter-twitter">Twitter</a> - <a href="<?= $baseUrl; ?>community/index/filter/github" id="filter-github">Github</a> </div>
+                <div class="filter">Filter: <a href="<?= $baseUrl; ?>community/" class="filter-all">All</a> - <a href="<?= $baseUrl; ?>community/index/filter/twitter" class="filter-twitter">Twitter</a> - <a href="<?= $baseUrl; ?>community/index/filter/github" class="filter-github">Github</a> </div>
                 <div class="back-to-homepage"><a href="<?= $baseUrl; ?>">Back to Homepage</a></div>
         </div>
 
