98cd635018028529b0a2557abbf1057b71b71723
Paul Dragoonis <dragoonis@gmail.com>
2011-08-05 17:01:56 +0100

    Wrapping the code in a JQ ready statement

--- scripts/community.js
+++ scripts/community.js
@@ -1,18 +1,20 @@
-$(".topcontent .filter-twitter").click(function() {
-    $(".source-github").fadeOut();
-    $(".source-twitter").fadeIn();
-    return false;
-});
+jQuery(document).ready(function() {
+	$(".topcontent .filter-twitter").click(function() {
+		$(".source-github").fadeOut('fast');
+		$(".source-twitter").fadeIn('fast');
+		return false;
+	});
 
 
-$(".topcontent .filter-github").click(function() {
-    $(".source-github").fadeIn();
-    $(".source-twitter").fadeOut();
-    return false;
-});
+	$(".topcontent .filter-github").click(function() {
+		$(".source-github").fadeIn('fast');
+		$(".source-twitter").fadeOut('fast');
+		return false;
+	});
 
-$(".topcontent .filter-all").click(function() {
-    $(".source-github").fadeIn();
-    $(".source-twitter").fadeIn();
-    return false;
+	$(".topcontent .filter-all").click(function() {
+		$(".source-github").fadeIn('fast');
+		$(".source-twitter").fadeIn('fast');
+		return false;
+	});
 });
