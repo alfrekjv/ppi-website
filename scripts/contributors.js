955409420fc6672b058bf81ca4010568bff24a08
Alfredo Juarez <alfrekjv@gmail.com>
2011-08-11 11:47:36 -0600

    fixed toggle link

--- scripts/contributors.js
+++ scripts/contributors.js
@@ -1,7 +1,16 @@
 jQuery(document).ready(function() {
-
+    var hidden = null;
+    
 	$('.contributors-item .links a').click( function() {
 		$(this).parents('.contributors-item').find('.committers').slideToggle();
+
+        if (!hidden) {
+            $(this).html("Hide Contributors");
+            hidden = true;
+        } else {
+            $(this).html("Show Contributors");
+        }
+
 		return false;
 	});
 
