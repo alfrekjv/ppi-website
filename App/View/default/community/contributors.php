95b6bf27142a6c3a1c5d8dd13bf6a9e561ed8b5d
Paul Dragoonis <dragoonis@gmail.com>
2011-08-12 00:17:16 +0100

    Finished the contributors page. Filling in all the people who deserve credit

--- App/View/default/community/contributors.php
+++ App/View/default/community/contributors.php
@@ -7,6 +7,50 @@
 	</div>
 
 	<div class="contributors-item-container">
+
+        <div class="contributors-item">
+			<div class="header">
+				<img class="fl logo" src="<?= $baseUrl; ?>images/icons-download.png">
+				<div class="fl">
+					<div class="project-title">Advisory Team</div>
+					<div class="project-shortdesc">The Wise Monks - Helping direct the future of PPI</div>
+				</div>
+				<div class="links">
+					<a href="#" title="Show Contributors">Show Contributors</a>
+				</div>
+				<div class="clear"></div>
+			</div>
+            <div class="committers">
+				<div class="committer">
+					<p class="name">Ryan Mauger (ryan@rmauger.co.uk) - <a href="http://twitter.com/#!/bittarman">Twitter</a> - <a href="http://twitter.com/#!/bittarman">Github</a> - <a href="http://www.rmauger.co.uk/">Website</a></p>
+				</div>
+				<div class="committer">
+					<p class="name">David Rogers (david@orlandophp.org) - <a href="http://twitter.com/#!/al_the_x">Twitter</a> - <a href="https://github.com/al-the-x">Github</a> - <a href="http://ethos-development.com">Website</a></p>
+				</div>
+				<div class="committer">
+					<p class="name">Jordi Boggiano (seldaek@php.net) - <a href="http://twitter.com/#!/seldaek">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
+				</div>
+                <div class="committer">
+                    <p class="name">Guilherme Blanco (guilhermeblanco@php.net) - <a href="http://twitter.com/#!/guilhermeblanco">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
+                </div>
+				<div class="committer">
+					<p class="name">Davey Shafik (davey@php.net) - <a href="http://twitter.com/#!/dshafik">Twitter</a> - <a href="https://github.com/dshafik">Github</a></p>
+				</div>
+                <div class="committer">
+                    <p class="name">Cal Evans (cal@calevans.com) - <a href="http://twitter.com/#!/CalEvans">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
+                </div>
+                <div class="committer">
+                    <p class="name">Mark Schaschke (mark@fractalturtle.com) - <a href="http://twitter.com/#!/epitomus">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
+                </div>
+				<div class="committer">
+                    <p class="name">John Mertic (jmertic@php.net) - <a href="http://twitter.com/#!/jmertic">Twitter</a> - <a href="https://github.com/jmertic">Github</a> - <a href="http://jmertic.wordpress.com/">Website</a></p>
+				</div>
+                <div class="committer">
+                    <p class="name">Juozas Kaziukėnas (juozas@juokaz.com) - <a href="http://twitter.com/#!/juokaz">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
+                </div>
+            </div>
+		</div>
+
         <div class="contributors-item">
 			<div class="header">
 				<img class="fl logo" src="<?= $baseUrl; ?>images/icons-download.png">
@@ -21,32 +65,36 @@
 			</div>
             <div class="committers">
                  <div class="committer">
-                    <p class="name">Paul Dragoonis - <a href="http://twitter.com/dr4goonis">Twitter</a> - <a href="http://github.com/dragoonis">Github</a> - <a>Website</a></p>
-	                <p>Paul makes alfredo fly</p>
+                    <p class="name">Paul Dragoonis (dragoonis@php.net) - <a href="http://twitter.com/dr4goonis">Twitter</a> - <a href="http://github.com/dragoonis">Github</a> - <a>Website</a></p>
+	                <p>Project Leader</p>
                 </div>
                 <div class="committer">
-                    <p class="name">Alexy Shien (conf) - <a href="#">Twitter</a> - <a href="#">Github</a> - <a>Website</a></p>
-	                <p>Lorem ipsum dolor...</p>
+                    <p class="name">Anthony Van de Gejuchte (anthonyvdg@hotmail.com) - <a href="http://twitter.com/avdg">Twitter</a> - <a href="http://github.com/avdg">Github</a> - <a>Website</a></p>
+	                <p>Primarily known for his work on PPI_Request and PHPUnit testing</p>
                 </div>
                 <div class="committer">
-                    <p class="name">Anthony Van de Gejuchte - <a href="http://twitter.com/avdg">Twitter</a> - <a href="http://github.com/avdg">Github</a> - <a>Website</a></p>
-	                <p>Lorem ipsum dolor...</p>
+                    <p class="name">Alexy Shien (confik@gmail.com) - <a href="http://twitter.com/#!/shein_alexey">Twitter</a> - <a href="http://github.com/conf">Github</a> - <a>Website</a></p>
+	                <p>Important contributor to PPI_Form and PHPUnit testing</p>
+                </div>
+	            <div class="committer">
+                    <p class="name">Yousif Masoud (yousif.masoud@gmail.com) - <a href="http://twitter.com/#!/ymasoud">Twitter</a> - <a href="https://github.com/YMas">Github</a></p>
+	                <p>Major contributions to the PPI_DataSource component</p>
                 </div>
                 <div class="committer">
                     <p class="name">Alfredo Juarez - <a href="http://twitter.com/alfrekjv">Twitter</a> - <a href="http://github.com/alfrekjv">Github</a></p>
-	                <p>Alfredo makes shiny ponies fly</p>
-                </div>
-                <div class="committer">
-                    <p class="name">Anthony Fererra - <a href="#">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
-	                <p>Lorem ipsum dolor...</p>
+	                <p>Known for his work on PPI_Crud</p>
                 </div>
                 <div class="committer">
                     <p class="name">Robert Eisele - <a href="#">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
-	                <p>Lorem ipsum dolor...</p>
+<!--	                <p>tbc</p>-->
                 </div>
                 <div class="committer">
-                    <p class="name">Daniel Egeberg - <a href="#">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
-	                <p>Lorem ipsum dolor...</p>
+                    <p class="name">Daniel Egeberg (degeberg@php.net) - <a href="#">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
+	                <p>Influential to the scalability of database resultsets</p>
+                </div>
+                <div class="committer">
+                    <p class="name">Johnny Mast (mastjohnny@gmail.com) - <a href="#">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
+	                <p>Co-Founder of the pre-alpha version of PPI in 2007. Recently kicking things off with PHPUnit integration</p>
                 </div>
             </div>
 		</div>
@@ -64,12 +112,16 @@
 			</div>
             <div class="committers">
                  <div class="committer">
-                    <p class="name">Paul Dragoonis - <a href="http://twitter.com/dr4goonis">Twitter</a> - <a href="http://github.com/dragoonis">Github</a> - <a>Website</a></p>
-	                <p>Paul makes alfredo fly</p>
+                    <p class="name">Paul Dragoonis (dragoonis@php.net) - <a href="http://twitter.com/dr4goonis">Twitter</a> - <a href="http://github.com/dragoonis">Github</a> - <a>Website</a></p>
+	                <p>Project Leader</p>
                 </div>
                 <div class="committer">
-                    <p class="name">Ross Moroney - <a href="#">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
-	                <p>Lorem ipsum dolor...</p>
+                    <p class="name">Ross Moroney (code.ph0y@googlemail.com) - <a href="https://github.com/rossmoroney">Github</a></p>
+	                <p>PPI Installer work</p>
+                </div>
+                <div class="committer">
+                    <p class="name">Ryan Cummins (ryan@zenmedia.co.uk) - <a href="https://github.com/zenmedia">Github</a></p>
+	                <p>PPI Installer work</p>
                 </div>
             </div>
 		</div>
@@ -88,8 +140,16 @@
 			</div>
             <div class="committers">
                  <div class="committer">
-                    <p class="name">Paul Dragoonis - <a href="http://twitter.com/dr4goonis">Twitter</a> - <a href="http://github.com/dragoonis">Github</a> - <a>Website</a></p>
-	                <p>Paul makes alfredo fly</p>
+                    <p class="name">Paul Dragoonis (dragoonis@php.net) - <a href="http://twitter.com/dr4goonis">Twitter</a> - <a href="http://github.com/dragoonis">Github</a> - <a>Website</a></p>
+	                <p>Project Leader</p>
+                </div>
+                <div class="committer">
+                    <p class="name">Ross Moroney (code.ph0y@googlemail.com) - <a href="https://github.com/rossmoroney">Github</a></p>
+	                <p>Major contributions to most parts of the tracker. Development of the admin panel to manage issues.</p>
+                </div>
+                <div class="committer">
+                    <p class="name">Eric Couturier Guillaume (eric@eric-cg.fr) - <a href="http://twitter.com/#!/eric2145">Twitter</a> - <a href="https://github.com/Eric-CG">Github</a> - <a href="http://www.sydec-group.com">Website</a></p>
+	                <p>Project Maintenance. Integration with Github</p>
                 </div>
             </div>
 		</div>
@@ -107,8 +167,8 @@
 			</div>
             <div class="committers">
                  <div class="committer">
-                    <p class="name">Paul Dragoonis - <a href="http://twitter.com/dr4goonis">Twitter</a> - <a href="http://github.com/dragoonis">Github</a> - <a>Website</a></p>
-	                <p>Paul makes alfredo fly</p>
+                    <p class="name">Paul Dragoonis (dragoonis@php.net) - <a href="http://twitter.com/dr4goonis">Twitter</a> - <a href="http://github.com/dragoonis">Github</a> - <a>Website</a></p>
+	                <p>Project Leader</p>
                 </div>
             </div>
 		</div>
@@ -126,16 +186,24 @@
 			</div>
             <div class="committers">
                 <div class="committer">
-                    <p class="name">Alfredo Juarez - <a href="http://twitter.com/alfrekjv">Twitter</a> - <a href="http://github.com/alfrekjv">Github</a></p>
-	                <p>Alfredo makes shiny ponies fly</p>
+                    <p class="name">Paul Dragoonis (dragoonis@php.net) - <a href="http://twitter.com/dr4goonis">Twitter</a> - <a href="http://github.com/dragoonis">Github</a> - <a>Website</a></p>
+	                <p>Project Leader</p>
                 </div>
                 <div class="committer">
-                    <p class="name">Paul Dragoonis - <a href="http://twitter.com/dr4goonis">Twitter</a> - <a href="http://github.com/dragoonis">Github</a> - <a>Website</a></p>
-	                <p>Paul makes alfredo fly</p>
+                    <p class="name">Alfredo Juarez (alfredojv@juvasoft.com) - <a href="http://twitter.com/alfrekjv">Twitter</a> - <a href="http://github.com/alfrekjv">Github</a> - <a href="http://juvasoft.com">Website</a></p>
+	                <p>Alfredo has helped make the Contributors and Community pages</p>
                 </div>
                 <div class="committer">
-                    <p class="name">Ricky Stevens - <a href="#">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
-	                <p>Lorem ipsum dolor...</p>
+                    <p class="name">Ben Tadiar (me@bentadiar.co.uk) - <a href="http://twitter.com/#!/BenTheDesigner">Twitter</a> - <a href="https://github.com/BenTheDesigner">Github</a> - <a href="www.bentadiar.co.uk">Website</a></p>
+	                <p>Designer of the Downloads and Contributors pages</p>
+                </div>
+                <div class="committer">
+                    <p class="name">Ricky Stevens (ppi@ricky-stevens.com) - <a href="http://ricky-stevens.com/">Website</a></p>
+	                <p>Designer of the Community page</p>
+                </div>
+                <div class="committer">
+                    <p class="name">Eric Couturier Guillaume (eric@eric-cg.fr) - <a href="http://twitter.com/#!/eric2145">Twitter</a> - <a href="https://github.com/Eric-CG">Github</a> - <a href="http://www.sydec-group.com">Website</a></p>
+	                <p>General maintenance</p>
                 </div>
             </div>
 		</div>
@@ -145,7 +213,7 @@
 				<img class="fl logo" src="<?= $baseUrl; ?>images/icons-download.png">
 				<div class="fl">
 					<div class="project-title">API Documentation</div>
-					<div class="project-shortdesc">...</div>
+					<div class="project-shortdesc">The PPI Framework's API reference running on DocBlox.</div>
 				</div>
 				<div class="links">
 					<a href="#" title="Show Contributors">Show Contributors</a>
@@ -154,8 +222,8 @@
 			</div>
             <div class="committers">
                 <div class="committer">
-                    <p class="name">Mike Van Riel - <a href="#">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
-	                <p>Lorem ipsum dolor...</p>
+                    <p class="name">Mike Van Riel (mike.vanriel@naenius.com) - <a href="http://twitter.com/#!/mvriel">Twitter</a> - <a href="https://github.com/mvriel">Github</a> - <a href="http://blog.naenius.com">Website</a></p>
+	                <p>As the DocBlox author, Mike sets up our scheduled generation of the API.</p>
                 </div>
             </div>
 		</div>
@@ -164,8 +232,8 @@
 			<div class="header">
 				<img class="fl logo" src="<?= $baseUrl; ?>images/icons-download.png">
 				<div class="fl">
-					<div class="project-title">Advisory Team</div>
-					<div class="project-shortdesc">---</div>
+					<div class="project-title">Miscellaneous</div>
+					<div class="project-shortdesc">Random events that occur on the interweb</div>
 				</div>
 				<div class="links">
 					<a href="#" title="Show Contributors">Show Contributors</a>
@@ -174,44 +242,16 @@
 			</div>
             <div class="committers">
                 <div class="committer">
-                    <p class="name">Davey Shafik (davey@php.net) - <a href="#">Twitter</a> - <a href="#">Github</a></p>
-	                <p>...</p>
+                    <p class="name">Ryan Cummins (ryan@zenmedia.co.uk) - <a href="#">Github</a> - <a href="#">Website</a></p>
+	                <p>His company hosts all the PPI Framework websites. Bug Reports.</p>
                 </div>
                 <div class="committer">
-                    <p class="name">Mark Schaschke - <a href="#">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
-	                <p>Paul makes alfredo fly</p>
+                    <p class="name">Anthony Fererra (ircmaxell@php.net) - <a href="http://twitter.com/#!/ircmaxell">Twitter</a> - <a href="https://github.com/ircmaxell">Github</a> - <a href="http://blog.ircmaxell.com/">Website</a></p>
+	                <p>Security bug reports</p>
                 </div>
-                <div class="committer">
-                    <p class="name">Cal Evans - <a href="#">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
-	                <p>Lorem ipsum dolor...</p>
-                </div>
-                <div class="committer">
-                    <p class="name">David Rogers - <a href="#">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
-	                <p>Lorem ipsum dolor...</p>
-                </div>
-                <div class="committer">
-                    <p class="name">Guilherme Blanco - <a href="#">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
-	                <p>Lorem ipsum dolor...</p>
-                </div>
-                <div class="committer">
-                    <p class="name">John Mertic (jmertic@php.net) - <a href="#">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
-	                <p>Lorem ipsum dolor...</p>
-                </div>
-                <div class="committer">
-                    <p class="name">Jordi Boggiano (seldaek@php.net) - <a href="#">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
-	                <p>Lorem ipsum dolor...</p>
-                </div>
-                <div class="committer">
-                    <p class="name">Juozas - <a href="#">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
-	                <p>Lorem ipsum dolor...</p>
-                </div>
-                <div class="committer">
-                    <p class="name">Ryan Mauger - <a href="#">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
-	                <p>Lorem ipsum dolor...</p>
-                </div>
-                <div class="committer">
-                    <p class="name">Robert Eisele - <a href="#">Twitter</a> - <a href="#">Github</a> - <a href="#">Website</a></p>
-	                <p>Lorem ipsum dolor...</p>
+               <div class="committer">
+                    <p class="name">Mark Roduner (mark.roduner@gmail.com) - <a href="https://github.com/mroduner">Github</a></p>
+	                <p>Cache bug reports</p>
                 </div>
             </div>
 		</div>
--- scripts/contributors.js
+++ scripts/contributors.js
@@ -12,4 +12,6 @@ jQuery(document).ready(function() {
 		return false;
 	});
 
+	$('.contributors-item .links a:first').trigger('click');
+
 });
