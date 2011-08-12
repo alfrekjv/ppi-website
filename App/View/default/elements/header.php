d1021de3f545c24d225dee176f0eb5de66864f9b
Paul Dragoonis <dragoonis@gmail.com>
2011-08-12 22:43:01 +0100

    Changing the dimensions of the gplusone element

--- App/View/default/elements/header.php
+++ App/View/default/elements/header.php
@@ -6,7 +6,7 @@
 			<div class="facebooklike-container">
 			        <iframe src="http://www.facebook.com/plugins/like.php?app_id=260678113958116&amp;href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FPPI-Framework%2F121479501280862&amp;send=false&amp;layout=box_count&amp;width=100&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font=verdana&amp;height=65" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:65px;" allowTransparency="true"></iframe>
 			</div>
-        	<div class="gplusone-container"><g:plusone></g:plusone></div>
+        	<div class="gplusone-container"><g:plusone size="tall"></g:plusone></div>
 	        <div class="twitterlike-contaner">
 		        <a href="http://twitter.com/share" class="twitter-share-button" data-count="vertical" data-via="ppi_framework">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
 	        </div>
